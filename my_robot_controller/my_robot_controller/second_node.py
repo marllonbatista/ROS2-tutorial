5600b0821ae1d8ab6ae57d8c713b26d005a202a3
Marllon Batista <marllonbatista.12@gmail.com>
2023-06-10 12:49:59 -0300

    feat:finished second_node

--- my_robot_controller/my_robot_controller/second_node.py
+++ my_robot_controller/my_robot_controller/second_node.py
@@ -1,12 +1,16 @@
 
 import rclpy
 from rclpy.node import Node
-
+from turtlesim.msg import Pose
 
 class position_node(Node):
     def __init__(self): 
         super().__init__("second_node")
-        self.get_logger().info("start!")
+        self.pose_subscriber=self.create_subscription(Pose,"/turtle1/pose",self.pose_callback,10)
+        self.pose_subscriber
+        
+    def pose_callback(self,msg:Pose):
+        self.get_logger().info(str(msg.x))
 def main(args=None):
     rclpy.init(args=args)
     node=position_node()
