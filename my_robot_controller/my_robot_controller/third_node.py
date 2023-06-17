18a9db8b974157079e9096afdc8b9c4a69e363a6
Marllon Batista <marllonbatista.12@gmail.com>
2023-06-17 19:48:51 -0300

    feat:finished third node

--- my_robot_controller/my_robot_controller/third_node.py
+++ my_robot_controller/my_robot_controller/third_node.py
@@ -6,7 +6,25 @@ from turtlesim.msg import Pose
 class turtlecontroller (Node):
     def __init__(self) :
         super().__init__("third_node")
-        self.get_logger().info("start")
+        self.publi=self.create_publisher(Twist,"/turtle1/cmd_vel",10)
+        self.subscript=self.create_subscription(Pose,"/turtle1/pose",self.controller_callback,10)
+    def controller_callback(self,pose:Pose):
+        
+        msg_vel=Twist()
+        if pose.x < 10.0 and pose.y<5.55:
+            msg_vel.linear.x=2.0
+            msg_vel.linear.y=0.0
+        elif pose.x>10.0 and pose.y<10.0:
+            msg_vel.linear.x=0.0
+            msg_vel.linear.y=2.0
+        elif pose.y >10.0 and pose.x > 5.5:
+            msg_vel.linear.x=-2.0
+            msg_vel.linear.y=0.0
+        elif pose.x<5.5 and pose.y > 5.6:
+            msg_vel.linear.x=0.0
+            msg_vel.linear.y=-2.0
+        self.publi.publish(msg_vel)
+        
 def main(args=None):
     rclpy.init(args=args)
     node=turtlecontroller()
